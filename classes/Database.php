74226c9a8c0f763860dbfd1d441ed664270b5a2b
Wei Yang <weix0015@stud.kea.dk>
2025-04-15 14:06:04 +0200

    removed procfile and heroku setup

--- classes/Database.php
+++ classes/Database.php
@@ -8,7 +8,6 @@ Class Database extends DBCredentials
 
   public function __construct()
   {
-    parent::__construct();
 
     $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
     $options = [
@@ -16,11 +15,7 @@ Class Database extends DBCredentials
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ];
 
-    try {
-      $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
-    } catch (PDOException $e) {
-      die("Database connection failed: " . $e->getMessage());
-    }
+    $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
   }
 
   public function __destruct()
