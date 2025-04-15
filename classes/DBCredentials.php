ba371bf291a09661f0bc52989d48b44fd329fb94
Wei Yang <weix0015@stud.kea.dk>
2025-04-15 13:59:47 +0200

    fikset jawsdb

--- classes/DBCredentials.php
+++ classes/DBCredentials.php
@@ -1,9 +1,34 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 Class DBCredentials
 {
-  protected string $host = 'localhost';
-  protected string $dbname = 'company';
-  protected string $user = 'root';
+  protected string $host = '';
+  protected string $dbname = '';
+  protected string $user = '';
   protected string $password = '';
+
+  public function __construct()
+  {
+    $jawsdb_url = getenv('JAWSDB_URL');
+
+    if ($jawsdb_url) {
+      $url_parts = parse_url($jawsdb_url);
+
+      $this->host = $url_parts['host'];
+      $this->dbname = ltrim($url_parts['path'], '/');
+      $this->user = $url_parts['user'];
+      $this->password = $url_parts['pass'];
+    } else {
+      $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+      $dotenv->safeLoad();
+
+      $this->host = $_ENV['DB_HOST'] ?? 'localhost';
+      $this->dbname = $_ENV['DB_DATABASE'] ?? 'company';
+      $this->user = $_ENV['DB_USERNAME'] ?? 'root';
+      $this->password = $_ENV['DB_PASSWORD'] ?? '';
+      error_log("Warning: JAWSDB_URL not found. Using local .env settings.");
+    }
+  }
 }
--- classes/Database.php
+++ classes/Database.php
@@ -8,13 +8,19 @@ Class Database extends DBCredentials
 
   public function __construct()
   {
+    parent::__construct();
+
     $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
     $options = [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ];
 
-    $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
+    try {
+      $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
+    } catch (PDOException $e) {
+      die("Database connection failed: " . $e->getMessage());
+    }
   }
 
   public function __destruct()
